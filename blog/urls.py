e9740cb116c1be8a2db5f4e1e8c96c62f1c122dd
javiquiles <javiquiles@hotmail.com>
2017-04-10 20:15:08 -0300

    Vista con clases y plantilla base

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from . import views
+from blog import views
 
 urlpatterns = [
     url(r'^$', views.post_list),
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.views.generic import TemplateView, ListView
 from .models import Post
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# Vistas con Funciones.
+#def post_list(request):
+#    posts = Post.objects.all()
+#    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+#Vistas con Clases.
+class PostList(ListView):
+    model = Post
+
+post_list = PostList.as_view()
