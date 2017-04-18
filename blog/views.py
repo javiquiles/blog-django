5167c4038f45467f1b585dcf419f25a5e375621c
javiquiles <javiquiles@hotmail.com>
2017-04-18 16:57:22 -0300

    Nuevo template post_detail

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Vistas con Funciones.
@@ -8,9 +9,18 @@ from .models import Post
 #    posts = Post.objects.all()
 #    return render(request, 'blog/post_list.html', {'posts': posts})
 
+#def post_detail(request, pk):
+#    post = get_object_or_404(Post, pk=pk)
+#    return render(request, 'blog/post_detail.html', {'post': post})
+
 
 #Vistas con Clases.
 class PostList(ListView):
     model = Post
 
+class PostDetail(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+
 post_list = PostList.as_view()
+post_detail = PostDetail.as_view()
